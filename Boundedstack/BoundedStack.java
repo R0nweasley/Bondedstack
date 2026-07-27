5288a697dffbebf4d8fabedcfa61db2deb9374e1
R0nweasley <phakin.srih@ku.th>
2026-07-28 02:11:30 +0700

    Add files via upload

--- Boundedstack/BoundedStack.java
+++ Boundedstack/BoundedStack.java
@@ -1,10 +1,144 @@
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 /**
  *  ภาคิน ศรีเหรา 6821601348
  *  วรชิต วสุธนพิริยะ 6821601402
  *  
- *  BoundedStack is ...
+ *  BoundedStack is ตัวเก็บชุดข้อมูลแบบแนวตั้ง
+ *  
+ *  ex. [   "C"
+ *          "B"
+ *          "A"   ]
+ * 
+ *  BounedStack ตัวอย่างการใช้งาน
+ *  push()   เพิ่มข้อมูลไว้บนสุด
+ *  pop()    ลบตัวบนสุด
+ *  peek()   อ่านข้อมูลตัวบนสุด
+ *  isEmty() เช็กว่าว่างมั้ย
+ *  
+ *  
+ * Creators / Producers / Observers / Mutators
+ * Representation Invariant (RI) และตรวจสอบด้วย checkRep()
  */
 
 public class BoundedStack {
-    
+        
+        public static final int Bound = 10  ;
+        
+         // ===== representation =====
+        private final List<String> Book;   
+        /**
+         * Abstraction Function:
+         * AF(info) = ลำดับข้อมูล เช่น [info1 , info2 , info3] โดยลำดับมีความหมาย — ข้อมูลต้องเรียงโชว์ตามลำดับ
+         * 
+         * 
+         * 
+         * Representation Invariant(RI):
+         * 1 ห้าม Null
+         * 2 ไม่เป็นสตริงว่าง ""
+         * 3 Book ไม่ซ้ำกัน
+         * 4 ใส่ข้อมูลไม่เกิน Bound (ขอบเขต)
+         * 
+      
+
+         // ===== Checkrep =====
+        /*
+           แปลง  RI --> assert    
+         */
+        public void checkRep(){
+            assert Book != null : "Book's not null";
+            assert Book.size() <= Bound;
+            Set<String> seen = new HashSet<>();
+            for (String s : Book) {
+            assert s!=null ;
+            assert !(s=="") ;
+            assert seen.add(s);
+        }
+        
+    }
+
+
+        // ===== Creator =====
+        /*
+            สร้างที่เก็บหนังสือ boundedStack
+        */
+
+        public BoundedStack(){
+            this.Book = new ArrayList<>();
+            checkRep();
+            System.out.println(Book.size());
+        }
+
+         // ===== Creator 2 =====
+        /*
+            รับชื่อหนังสือมาสร้างที่เก็บ
+         */
+
+        public BoundedStack(List<String> name_book){
+            if (name_book == null||name_book.size()>Bound ) throw new IllegalArgumentException();
+            Set<String> seen = new HashSet<>();
+            for (String s : name_book) {
+            if(s==null||s=="") throw new IllegalArgumentException();
+            if(!seen.add(s)) throw new IllegalArgumentException();
+        }
+
+        this.Book = new ArrayList<>(name_book);  // แก้บรรทัดนี้
+        checkRep();
+        }
+  
+
+        //  ===== Mutators เพิ่มสมาชิก=====
+        /*
+        @param element สมาชิกที่จะเพิ่ม
+        @throws IllegalArgumentException ถ้า element เป็น null
+        @throws IllegalStateException ถ้า stack เต็มแล้ว
+         */
+        public void push(String name_book){
+            if (name_book == null) throw new IllegalArgumentException(); // != null 
+            if (Book.contains(name_book))  throw new IllegalStateException(); //ไม่ซ้ำ
+            if (Book.size()==Bound)  throw new IllegalStateException(); // เกินขอบเขต
+           
+            Book.add(name_book);
+            checkRep();
+        }
+
+
+        //  ===== Mutators 2 ลบสมาชิกตัวบนสุด =====
+        /*
+        @throws IllegalStateException ถ้า stack ไม่มีอะไรให้ลบ
+         */
+        public void pop(){
+            if(Book.isEmpty()) throw new IllegalStateException();
+
+            Book.remove(Book.size()-1); // ลบหนังสือตัวบนสุด ต้อง -1 เพราะอาเรย์นับเป็น 0 1 2 
+            checkRep();
+        }
+
+
+        //  =====Observers =====
+        public String peek(){
+            return Book.get(Book.size()-1);
+        }
+        public Boolean isEmpty(){
+            return Book.isEmpty();
+        }
+        public Boolean isFull(){
+            return Book.size() == Bound;
+        }
+
+
+
+        // ไม่เกี่ยวสร้างมาเทสเอง
+        public int getsize(){
+            return Book.size();
+        }
+
+
+
+        //  ===== Producer =====
+
+        
 }
--- Boundedstack/Test_runner.java
+++ Boundedstack/Test_runner.java
@@ -1,3 +1,87 @@
+import java.util.List;
+
 public class Test_runner {
+
+    private static int pass = 0;
+    private static int fail = 0;
+
+
+
+     private static void check(String name, boolean condition) {
+        if (condition) {
+            pass++;
+            System.out.println("[PASS] " + name);
+        } else {
+            fail++;
+            System.out.println("[FAIL] " + name);
+        }
+    }
+
+
+
+
+     public static void main(String[] args) {
+        boolean assertsOn = false;
+        assert assertsOn = true;
+        if (!assertsOn) {
+            System.out.println("WARNING: assertions disabled"
+                    + " - re-run with: java -ea PlaylistTest\n");
+        }
+
+        // TEST เอง
+        BoundedStack a = new BoundedStack();
+        a.push("Harry");
+        System.out.println(a.getsize());
+        a.push("ASD");
+        a.push("null");
+         System.out.println(a.getsize());
+        a.pop();
+        System.out.println(a.getsize());
+        System.out.println(a.peek());
+        a.push("lookkaree");
+        System.out.println(a.peek());
+        System.out.println(a.isEmpty());
+
+        System.out.println("=== Playlist Test Suite ===\n");
+
+        testCreators();
+        testAdd();
+        testRemove();
+        testObservers();
+        testProducer();
+        testExposure();
+
+        System.out.println("\n=== Summary ===");
+        System.out.println("pass: " +pass);
+        System.out.println("fail: " + fail);
+        System.out.println("Total : " + (pass + fail));
+        System.out.println(fail == 0 ? "ALL TESTSpass" : "SOME TESTS fail");
+
+        if (fail > 0) {
+            System.exit(1);
+        }
+    }
+
+    private static void testCreators(){
+        
+    }
+    private static void testAdd(){
+
+    }
+    private static void testRemove(){
+
+    }
+    private static void testObservers(){
+
+    }
+    private static void testProducer(){
+
+    }
+    private static void testExposure(){
+
+    }
+
     
 }
+
+
